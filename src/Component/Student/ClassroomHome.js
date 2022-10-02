5bc62a4c91a757f0e0b7703f44e208c82ed8870f
devdubu <insomnis2020@naver.com>
2022-10-02 20:01:54 +0900

    classroom/home 진행중

--- src/Component/Student/ClassroomHome.js
+++ src/Component/Student/ClassroomHome.js
@@ -65,7 +65,7 @@ export default function ClassroomHome() {
                                     </div>
                                 </div>
                                 <div className="w-36 h-56 mr-3 mt-1">
-                                    <div className="pt-3 pr-3 h-16 m-auto w-50 bg-stone-100 rounded-r-xl">
+                                    <div className="pt-3  pr-3 h-16 m-auto w-50 bg-stone-100 rounded-r-xl">
                                         <div className="mb-1 text-base font-medium dark:text-black">3주차</div>
                                         <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
                                             <div className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-200" style={{width: '45%'}}></div>
@@ -115,7 +115,7 @@ export default function ClassroomHome() {
                                         </div>
                                     </div>
                                     <div className="w-36 h-56 mr-3 mt-1">
-                                        <div className="pt-3 pr-3 h-16 m-auto w-48 bg-stone-100 rounded-xl">
+                                        <div className="pt-3 pr-3 pl-3 h-16 m-auto w-48 bg-stone-100 rounded-xl">
                                             <div className="mb-1 text-base font-medium dark:text-black">3주차</div>
                                             <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
                                                 <div className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-200" style={{width: '45%'}}></div>
@@ -143,19 +143,38 @@ export default function ClassroomHome() {
                     ********************* 시험 ************************
                     **************************************************
                     */}
-                    <div>
-                        <div>
-                            <h1>시험</h1>
-                        </div>
-                        <div>
+                    <div className="grid grid-cols-3">
+                        <div className="w-80 h-96">
                             <div>
                                 <div>
-                                    <h2>진행 예정</h2>
+                                    <p className="text-xl">과제</p>
                                 </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <h2>결과</h2>
+                                <div className="rounded-xl border-2 w-80 h-720 mt-3 pt-3 grid grid-cols-3 drop-shadow-lg">
+                                    <div className="h-56 w-24 ml-6 mt-1 mb-3">
+                                        <div className="pt-6 h-24 w-14 text-stone-700 bg-stone-100 rounded-xl">
+                                            <FontAwesomeIcon className="text-3xl " icon={faCalendarWeek} />
+                                            <p className="text-xs">진행 예정</p>
+                                        </div>
+                                        <div>
+                                            <div className="pt-6 mt-3 h-24 text-stone-700 w-14 bg-stone-100 rounded-xl">
+                                                <FontAwesomeIcon className="text-3xl" icon={faCalendarCheck}/>
+                                                <p className="text-xs">완료</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="w-36 h-56 mr-3 mt-1">
+                                        <div className="pt-6 pl-3 pr-3 h-24 m-auto w-48 bg-stone-100 rounded-xl">
+                                            <div className="mb-1 text-base font-medium dark:text-black">3주차</div>
+                                            <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+                                                <div className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-200" style={{width: '45%'}}></div>
+                                            </div>
+                                        </div>
+                                        <div className="mt-3 h-24 w-48 m-auto bg-stone-100 rounded-xl">
+                                            <button type="button" className="mt-4 ml-3 text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-1.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
+                                                출석
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
